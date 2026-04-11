c445658bc44a1913c2aa1da67615f212af1ca965
SoEndo <sqald@mail.4nintogame.com>
2026-04-11 11:05:07 +0900

    【追加】gitignoreの追加
    ・MacとPythonに対応するgitignoreファイルを作成
    ・connect.shをビルドと実行を分けられるように変更

--- enviroments/connect.sh
+++ enviroments/connect.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-docker compose up -d --build
+if [ "$1" = "renew" ]; then
+    docker compose up -d --build
+else
+    docker compose up -d
+fi
+
 docker exec -it iki_project_2026 /bin/bash
